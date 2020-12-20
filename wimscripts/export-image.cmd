b70ac258a1591018ac3b8758b5cda579337357ab
Michael H.G. Schmidt <michael@schmidt2.de>
2020-12-20 17:14:56 +0100

    .

--- wimscripts/export-image.cmd
+++ wimscripts/export-image.cmd
@@ -1,3 +1,8 @@
 del c:\TEMP\install.wim
-dism /Export-Image /SourceImageFile:C:\TEMP\sources\install.esd /SourceIndex:6 /DestinationImageFile:c:\TEMP\install.wim /Compress:Max /CheckIntegrity
+dism /Export-Image ^
+  /SourceImageFile:c:\TEMP\Win10\sources\install.esd ^
+  /SourceIndex:6 ^
+  /DestinationImageFile:c:\TEMP\install.wim ^
+  /Compress:Max ^
+  /CheckIntegrity
 
