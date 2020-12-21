c392b4ddc51481665370da0d27b73630137824b6
Michael H.G. Schmidt <michael@schmidt2.de>
2020-12-21 10:45:53 +0100

    .

--- wimscripts/finish-image.cmd
+++ wimscripts/finish-image.cmd
@@ -1,13 +1,13 @@
 set SOURCES=c:\TEMP\Win10\sources
 set MNT=c:\TEMP\WIM
 
-dism /Image:%MNT% /Cleanup-Image /StartComponentCleanup /ResetBase
-dism /Unmount-Wim /MountDir:%MNT% /Commit
+rem dism /Image:%MNT% /Cleanup-Image /StartComponentCleanup /ResetBase
+rem dism /Unmount-Wim /MountDir:%MNT% /Commit
 
 dism /Export-Image ^
   /SourceImageFile:%SOURCES%\install.wim ^
-  /DestinationImageFile:%SOURCES%\install_FINAL.wim ^
+  /DestinationImageFile:%SOURCES%\install_FINAL.esd ^
   /SourceIndex:1 ^
-  /Compress:Max ^
+  /Compress:Recovery ^
   /CheckIntegrity
 
---
+++ wimscripts/remove-components.cmd
@@ -0,0 +1,35 @@
+set MNT="c:\TEMP\WIM"
+
+install_wim_tweak.exe /p %MNT% /c "Microsoft-Windows-Internet" /r
+install_wim_tweak.exe /p %MNT% /c "Adobe-Flash" /r
+install_wim_tweak.exe /p %MNT% /c "Microsoft-Windows-Cortana" /r
+install_wim_tweak.exe /p %MNT% /c "Microsoft-Windows-Search2" /r
+install_wim_tweak.exe /p %MNT% /c "Microsoft-Windows-SearchEngine" /r
+install_wim_tweak.exe /p %MNT% /c "Microsoft-Windows-RetailDemo" /r
+install_wim_tweak.exe /p %MNT% /c "Microsoft-Windows-ContactSupport" /r
+install_wim_tweak.exe /p %MNT% /c "Microsoft-PPIProjection" /r
+install_wim_tweak.exe /p %MNT% /c "Microsoft-Windows-Backup" /r
+install_wim_tweak.exe /p %MNT% /c "Windows-Defender" /r
+
+install_wim_tweak.exe /p %MNT% /c "Microsoft-Xbox" /r
+install_wim_tweak.exe /p %MNT% /c "Microsoft-Windows-TroubleShooting" /r
+install_wim_tweak.exe /p %MNT% /c "Microsoft-OneCore-TroubleShooting" /r
+install_wim_tweak.exe /p %MNT% /c "Microsoft-Windows-LanguageFeatures" /r
+install_wim_tweak.exe /p %MNT% /c "Microsoft-Windows-Legacy" /r
+install_wim_tweak.exe /p %MNT% /c "Microsoft-OneCore-SpeechComponents" /r
+install_wim_tweak.exe /p %MNT% /c "Microsoft-OneCore-Gaming" /r
+install_wim_tweak.exe /p %MNT% /c "Microsoft-OneCore-Indexer" /r
+install_wim_tweak.exe /p %MNT% /c "Microsoft-Windows-BioEnrollment" /r
+install_wim_tweak.exe /p %MNT% /c "Microsoft-OneCore-Biometrics" /r
+
+install_wim_tweak.exe /p %MNT% /c "Microsoft-Windows-Printing-XPSServices" /r
+install_wim_tweak.exe /p %MNT% /c "Microsoft-Windows-Xps" /r
+install_wim_tweak.exe /p %MNT% /c "Microsoft-Windows-Geolocation" /r
+install_wim_tweak.exe /p %MNT% /c "Microsoft-Windows-Compression" /r
+install_wim_tweak.exe /p %MNT% /c "Microsoft-Windows-BusinessScanning" /r
+install_wim_tweak.exe /p %MNT% /c "Microsoft-Windows-MediaPlayer" /r
+install_wim_tweak.exe /p %MNT% /c "Microsoft-Windows-WindowsMediaPlayer" /r
+install_wim_tweak.exe /p %MNT% /c "Microsoft-Windows-VirtualPC" /r
+install_wim_tweak.exe /p %MNT% /c "Microsoft-Windows-VirtualXP" /r
+install_wim_tweak.exe /p %MNT% /c "Microsoft-Windows-SystemRestore" /r
+
