2c9efcd215f67501b706d456e30b9daff05f2968
Michael H.G. Schmidt <michael@schmidt2.de>
2020-12-24 14:38:22 +0100

    .

--- 1st-logon.cmd
+++ 1st-logon.cmd
@@ -23,6 +23,7 @@ mkdir %SCRIPTS% 1>nul 2>nul
 mkdir %SCRIPTS%\conf 1>nul 2>nul
 mkdir %SCRIPTS%\desktop 1>nul 2>nul
 mkdir %SCRIPTS%\tweaks 1>nul 2>nul
+mkdir %SCRIPTS%\other 1>nul 2>nul
 
 echo getting TOOLS from webserver ...
 ftp -i -s:D:\ToolsDownload.ftp
