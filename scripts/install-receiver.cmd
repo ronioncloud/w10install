98f82f7877c7dcfc52562a4fcccf37944fc7cf3e
Michael H.G. Schmidt <michael@schmidt2.de>
2021-01-21 00:29:50 +0100

    .

---
+++ scripts/install-receiver.cmd
@@ -0,0 +1,11 @@
+@echo off
+
+set EXE=..\software\CitrixReceiver_4.9.9002_LTSR.exe
+
+echo ####### %0 #######
+
+echo installing Citrix Receiver LTS ...
+%EXE% /silent /noreboot
+
+echo ####### %0 #######
+
