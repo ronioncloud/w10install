1f0f1a081fded294597c79a7aa9e281c1b5c5081
Michael H.G. Schmidt <michael@schmidt2.de>
2021-02-07 20:29:19 +0100

    .

---
+++ scripts/modify-desktoptheme.cmd
@@ -0,0 +1,11 @@
+@echo off
+
+echo ####### %0 #######
+
+echo activating AEROLITE theme ...
+@echo on
+powershell -command "Start-Process C:\Windows\Resources\Themes\aerolite.theme"
+@echo off
+
+echo ####### %0 #######
+
