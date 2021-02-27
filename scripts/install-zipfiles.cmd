d3ae20a390ef753956aa6a3d201ab514c2653151
Michael H.G. Schmidt <michael@schmidt2.de>
2021-02-27 23:43:40 +0100

    .

--- scripts/install-zipfiles.cmd
+++ scripts/install-zipfiles.cmd
@@ -1,19 +1,97 @@
-echo unpacking TOTAL commander ...
-%TOOLS%\7z x -aoa -o%TOOLS% %TOOLS%\totalcmd_951.zip
-del /F %TOOLS%\totalcmd_951.zip 1>nul 2>nul
+@echo off
+
+set TOOLS=c:\tools
+set SOFTWARE=..\software
+set T=c:\TEMP
+
+echo ####### %0 #######
+
+rem ###################################
+rem MAIN loop ( unzip archives ) ...
+rem ###################################
+
+for %%A in (
+
+  bginfo
+  curl
+  imapcopy
+  installwimtweak
+  offlineregistryfinder
+  rclone
+  restic
+  vim
+  winscp
+  wub
+
+) do (
+  
+  if NOT EXIST %SOFTWARE%\%MSI% (
+    echo WARNING: %SOFTWARE%\%%A.zip not found!
+  )
+
+  echo unpacking %%A.zip ...
+  %TOOLS%\7z e -aoa -o%T%\%%A %SOFTWARE%\%%A.zip
+
+)
+
+echo.
+echo ###################################
+echo move files to tools folder
+echo ###################################
 echo.
 
-echo unpacking notepad++ ...
-%TOOLS%\7z x -aoa -o%TOOLS% %TOOLS%\notepad++.zip
-del /F %TOOLS%\notepad++.zip 1>nul 2>nul
+echo BGinfo ...
+mkdir %TOOLS%\bginfo 2>nul
+move /Y %T%\bginfo\Bginfo64.exe %TOOLS%\bginfo
+rd /S /Q %T%\bginfo 2>nul
 echo.
 
-echo unpacking ImapCopy ...
-%TOOLS%\7z x -aoa -o%TOOLS% %TOOLS%\ImapCopy.zip
-del /F %TOOLS%\ImapCopy.zip 1>nul 2>nul
+echo CURL
+move /Y %T%\curl\curl.exe %TOOLS%
+move /Y %T%\curl\libcurl*.dll %TOOLS%
+move /Y %T%\curl\curl-ca-bundle.crt %TOOLS%
+rd /S /Q %T%\curl
 echo.
 
-echo unpacking MediaCreationTool ...
-%TOOLS%\7z x -aoa -o%SOFTWARE% %SOFTWARE%\MediaCreationTool20H2.zip
-del /F %SOFTWARE%\MediaCreationTool20H2.zip 1>nul 2>nul
+echo ImapCopy
+mkdir %TOOLS%\imapcopy 2>nul
+move /Y %T%\imapcopy\* %TOOLS%\imapcopy
+rd /S /Q %T%\imapcopy
 echo.
+
+echo InstallWimTweak
+move /Y %T%\installwimtweak\* %TOOLS%
+rd /S /Q %T%\installwimtweak
+echo.
+
+echo OfflineRegistryFinder
+move /Y %T%\offlineregistryfinder\*.exe %TOOLS%
+rd /S /Q %T%\offlineregistryfinder
+echo.
+
+echo RClone
+move /Y %T%\rclone\rclone.exe %TOOLS%
+rd /S /Q %T%\rclone
+echo.
+
+echo Restic
+move /Y %T%\restic\*.exe %TOOLS%\restic.exe
+rd /S /Q %T%\restic
+echo.
+
+echo VIM
+move /Y %T%\vim\vim.exe %TOOLS%
+rd /S /Q %T%\vim
+echo.
+
+echo WinSCP
+move /Y %T%\winscp\*.exe %TOOLS%
+rd /S /Q %T%\winscp
+echo.
+
+echo WUB - Windows update blocker
+move /Y %T%\wub\wub_x64.exe %TOOLS%\wub.exe
+rd /S /Q %T%\wub
+echo.
+
+echo ####### %0 #######
