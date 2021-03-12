04993e17ac86c52efb3730bb0ab69008d6f24224
Michael H.G. Schmidt <michael@schmidt2.de>
2021-03-13 00:40:26 +0100

    added cyberjack script

--- optional/install-cyberjack.cmd
+++ optional/install-cyberjack.cmd
@@ -1,6 +1,7 @@
 @echo off
 
 set EXE=..\software\cyberjack-setup.exe
+set STARTMENU="%PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs"
 
 if NOT EXIST %EXE% (
   echo ERROR: %EXE% not found!
@@ -10,7 +11,14 @@ if NOT EXIST %EXE% (
 echo ####### %0 #######
 
 echo installing printer ReinerSCT Cyberjack ...
-start %EXE%
+start /wait %EXE%
+
+echo changing cyberjack startmenu folder ...
+move /Y %STARTMENU%\"Reiner SCT cyberjack\cyberjack*.*" %STARTMENU%\CyberJack.lnk 2>nul
+
+echo removing cyberjack startmenu folder ...
+rd /S /Q %STARTMENU%\"Reiner SCT cyberjack"
 
 echo ####### %0 #######
 pause
+
--- optional/install-googledrive.cmd
+++ optional/install-googledrive.cmd
@@ -23,14 +23,10 @@ move /Y %STARTMENU%\"Backup and Sync from Google\Backup and Sync from Google.lnk
   %STARTMENU%\"Backup and Sync.lnk"
 
 echo removing google drive startmenu folder ...
-@echo on
 rd /S /Q %STARTMENU%\"Backup and Sync from Google"
-@echo off
 
 echo removing google desktop icons ...
-@echo on
-del /F /Q %PUBLIC%\Desktop\Google*
-@echo off
+del /F /Q %PUBLIC%\Desktop\Google* 2>nul
 
 rem refresh desktop (W10 style)
 ie4uinit.exe -show
