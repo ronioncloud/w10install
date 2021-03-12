6d1890d37a46f0f9c603c8523774e7a3b99619fc
Michael H.G. Schmidt <michael@schmidt2.de>
2021-03-13 00:51:26 +0100

    bugfix

--- optional/install-xojo.cmd
+++ optional/install-xojo.cmd
@@ -16,6 +16,9 @@ taskkill /F /IM xojo.exe 2>nul
 echo installing XOJO ...
 start /wait %EXE% /LOADINF=%CONFIG% /NORESTART /NOCANCEL /SILENT /SUPPRESSMSGBOXES /CLOSEAPPLICATIONS
 
+echo renaming XOJO desktop icon ...
+move /Y %PUBLIC%\Desktop\Xojo*.lnk %PUBLIC%\Desktop\XOJO.lnk 2>nul
+
 echo ####### %0 #######
 pause
 
