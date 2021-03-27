6af4c0767c915475feab9109be79a93d7c7f6b54
Michael H.G. Schmidt <michael@schmidt2.de>
2021-03-28 00:03:35 +0100

    esd / wim ???

--- wim/export-image.cmd
+++ wim/export-image.cmd
@@ -1,20 +1,30 @@
 @echo off
 set SOURCES=c:\TEMP\W10\sources
+set WIM=%SOURCES%\install.esd
 
-if NOT EXIST %SOURCES%\install.esd (
+if NOT EXIST %WIM% (
   echo.
-  echo [%0] ERROR: %SOURCES%\install.esd NOT found!
-  exit /b
+  echo [%0] WARNING: %WIM% NOT found!
+
+  rem search for "install.wim" ...
+  set WIM=%SOURCES%\install.wim
+  if NOT EXIST %WIM% (
+    echo [%0] ERROR: %WIM% also NOT found!
+    exit /b
+  ) else (
+    rem rename WIM file ...
+    move /Y %WIM% %SOURCES%\install-orig.wim
+    set WIM=%SOURCES%\install-orig.wim
 )
 
-echo deleting old install.wim ...
+echo deleting "old" install.wim ...
 del %SOURCES%\install.wim 1>nul 2>nul
 
-echo exporting image from %SOURCES%\install.esd ...
+echo exporting image from %WIM% ...
 dism /Export-Image ^
-  /SourceImageFile:%SOURCES%\install.esd ^
+  /SourceImageFile: %WIM% ^
   /DestinationImageFile:%SOURCES%\install.wim ^
   /SourceName:"Windows 10 Pro" ^
-  /Compress:Max ^
+  /Compress:None ^
   /CheckIntegrity
 
