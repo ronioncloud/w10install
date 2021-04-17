d6d8afec661da75c24d71374961846892beaddc2
Michael H.G. Schmidt <michael@schmidt2.de>
2021-04-17 11:36:27 +0200

    added update blocking rule for emclient

--- optional/install-emclient.cmd
+++ optional/install-emclient.cmd
@@ -17,18 +17,24 @@ msiexec /i %MSI% /quiet
 @echo off
 cd %OPT%
 
-rem copy link to desktop ...
-copy /Y "%PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs\eM Client.lnk" ^
-  %PUBLIC%\Desktop
-
-rem remove autostart for em-Client ...
+echo removing autostart for em-Client ...
 reg delete "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" ^
   /v "eM Client" /f
 
 taskkill /F /IM SystemSettings.exe 2>nul
 
-rem refresh desktop (W10 style)
+echo copying link to desktop ...
+copy /Y "%PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs\eM Client.lnk" ^
+  %PUBLIC%\Desktop
+
+echo refreshing desktop ...
 ie4uinit.exe -show
+
+echo disabling automatic updates via W10 firewall ...
+netsh advfirewall firewall delete rule name="block_emclient_updates" 2>nul
+netsh advfirewall firewall add rule name="block_emclient_updates" ^
+  dir=out action=block protocol=tcp localip=any remoteip=168.62.48.183 ^
+  remoteport=80,443 program="%ProgramFiles(x86)%\eM Client\MailClient.exe"
 
 echo ####### %0 #######
 pause
