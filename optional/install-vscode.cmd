54692a6a921812113bb6db26d95ae6c6acd9ad5e
Michael H.G. Schmidt <michael@schmidt2.de>
2021-04-25 20:42:05 +0200

    updates for VScode blocked via firewall.

--- optional/install-vscode.cmd
+++ optional/install-vscode.cmd
@@ -4,6 +4,9 @@ set CONFIG=config\install-vscode.txt
 set EXE=..\software\vscode-setup.exe
 set STARTMENU="%PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs"
 
+rem update.code.visualstudio.com
+set VSCODEUPDATEIP=51.144.164.215
+
 if NOT EXIST %EXE% (
   echo ERROR: %EXE% not found!
   exit /b
@@ -26,6 +29,13 @@ move /Y "%PUBLIC%\Desktop\Visual*.lnk" "%PUBLIC%\Desktop\VScode.lnk"
 
 rem refresh desktop (W10 style)
 ie4uinit.exe -show
+
+rem NO updates here! FUCK YOU!
+echo disabling automatic updates via W10 firewall ...
+netsh advfirewall firewall delete rule name="block_vscode_updates" 2>nul
+netsh advfirewall firewall add rule name="block_vscode_updates" ^
+  dir=out action=block protocol=tcp localip=any remoteip=%VSCODEUPDATEIP% ^
+  remoteport=80,443 program="%ProgramFiles%\Microsoft VS Code\Code.exe"
 
 echo ####### %0 #######
 pause
