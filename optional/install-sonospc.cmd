ff00e59fa689536f411864933a512d94ea539fcf
Michael H.G. Schmidt <michael@schmidt2.de>
2021-08-29 18:29:35 +0200

    .

--- optional/install-sonospc.cmd
+++ optional/install-sonospc.cmd
@@ -1,6 +1,7 @@
 @echo off
 
 set EXE=..\software\sonospc-setup.exe
+set STARTMENU="%PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs"
 
 if NOT EXIST %EXE% (
   echo ERROR: %EXE% not found!
@@ -12,8 +13,11 @@ echo ####### %0 #######
 echo installing SONOS PC client ...
 start /wait %EXE% /L1031 /S /v/qn
 
-rem echo removing F-Secure desktop link ...
-rem del /F "%PUBLIC%\Desktop\F-Secure Anti-Virus.lnk" 2>nul
+echo moving SONOS startmenu shortcut ...
+move /Y %STARTMENU%\"Sonos\Sonos.lnk" %STARTMENU%\Inkscape.lnk
+
+echo removing SONOS startmenu folder ...
+rd /S /Q %STARTMENU%\"Sonos" 2>nul
 
 rem refresh desktop (W10 style)
 ie4uinit.exe -show
