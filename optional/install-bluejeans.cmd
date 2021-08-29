cf774dafa9ce6bfde21e5f2b7b92ee8fa9807e58
Michael H.G. Schmidt <michael@schmidt2.de>
2021-08-29 18:06:38 +0200

    disabled autostart bluejeans

--- optional/install-bluejeans.cmd
+++ optional/install-bluejeans.cmd
@@ -21,6 +21,13 @@ cd %OPT%
 echo removing Desktop icon ...
 del /F "%USERPROFILE%\Desktop\BlueJeans.lnk" 2>nul
 
+echo removing autostart for BlueJeans-Client ...
+reg delete "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" ^
+  /v "BlueJeans.Detector" /f
+
+echo refreshing desktop (W10 style) ...
+ie4uinit.exe -show
+
 echo ####### %0 #######
 pause
 
